15addb4d211ffa9bb1231e60f7524c4430585892
Jiajie Chen <jiajie_chen@brown.edu>
2015-12-09 03:04:24 -0500

    neeeigh im a horse

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,30 +1,34 @@
-/*
-Coded by Marjan Olesch
-Sketch from Insctructables.com
-Open source - do what you want with this code!
-*/
+#include "motor.h"
+
+// TODO: Control these motors. 
 #include <Servo.h>
 
+int value = 0;
 
-int value = 0; // set values you need to zero
-
-Servo firstESC; //Create as much as Servoobject you want. You can controll 2 or more Servos at the same time
+Servo firstESC;
+motor_t m;
 
 void setup() {
   //pinMode(6, OUTPUT);
   firstESC.attach(6);    // attached to pin 9 I just do this with 1 Servo
+  makeMotor(6, m, firstESC);
   Serial.begin(115200);    // start serial at 9600 baud
 }
 
 void loop() {
-
-//First connect your ESC WITHOUT Arming. Then Open Serial and follo Instructions
- 
   //analogWrite(6, value);
-  firstESC.writeMicroseconds(value);
+  m.targetLevel = value;
+  tickMotor(m, firstESC);
  
   if(Serial.available()) {
     value = Serial.parseInt();    // Parse an Integer from Serial
+    Serial.print("Value: ");
     Serial.println(value);
   }
+  
+  Serial.print("Current: ");
+  Serial.println(m.currentLevel);
+  
+  delay(100);
 }
+
